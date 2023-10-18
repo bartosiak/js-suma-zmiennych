83ca250ebcc2a4931b97daa12db5283404052c91
Bartosiak <bartosiak.contact@gmail.com>
2023-10-18 10:50:14 +0200

    stworzenie pliku

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 5;
+let secondNumber = 10;
+
+function sum(a, b) {
+    let suma = a + b;
+    return suma;
+}
+
+let result = sum(firstNumber, secondNumber);
+console.log(result);
